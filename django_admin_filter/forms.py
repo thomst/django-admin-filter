83a37c3c9f8edcdb3eb7c796d72f08fe77d8dbf4
Thomas Leichtfuß <leichtfuss@systopia.de>
2020-05-03 13:44:07 +0200

    some formatting for the filter-form

--- django_admin_filter/forms.py
+++ django_admin_filter/forms.py
@@ -3,7 +3,20 @@ from .models import Filter
 
 
 class FilterForm(forms.ModelForm):
-    name = forms.CharField(required=False)
+    name = forms.CharField(
+        max_length=128,
+        required=False,
+        widget=forms.widgets.TextInput(
+            attrs=dict(placeholder='optional')
+        )
+    )
+    description = forms.CharField(
+        required=False,
+        widget=forms.widgets.Textarea(attrs=dict(
+            rows=4,
+            placeholder='optional'
+        ))
+    )
 
     class Meta:
         model = Filter
