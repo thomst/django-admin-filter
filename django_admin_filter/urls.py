52389d64307b2f37d79f98e7b5ccfbdaee7e26a5
pyMan <claudiomarinozzi78@gmail.com>
2021-01-04 16:18:32 +0100

    edit saved filters
    - edit saved filter
    - create new different filter from saved one

--- django_admin_filter/urls.py
+++ django_admin_filter/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import re_path
-from .views import FilterQueryView
+from .views import FilterQueryViewCreate, FilterQueryViewUpdate
 from . import settings
 
 
 urlpatterns = [
-    re_path(r'^(?P<app_label>\w+)/(?P<model>\w+)/{}$'.format(settings.URL_PATH), FilterQueryView.as_view()),
-    re_path(r'^(?P<app_label>\w+)/(?P<model>\w+)/{}(?P<pk>\d+)/$'.format(settings.URL_PATH), FilterQueryView.as_view()),
+    re_path(r'^(?P<app_label>\w+)/(?P<model>\w+)/{}$'.format(settings.URL_PATH), FilterQueryViewCreate.as_view()),
+    re_path(r'^(?P<app_label>\w+)/(?P<model>\w+)/{}(?P<pk>\d+)/$'.format(settings.URL_PATH), FilterQueryViewUpdate.as_view()),
 ]
